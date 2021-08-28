478fb78d419c013fb7ad31979bc52ca869ea34ce
Khadim <khadimfall0602@gmail.com>
2021-08-28 19:17:32 +0200

    soime updates

--- lib/models/OEXEngine.dart
+++ lib/models/OEXEngine.dart
@@ -10,31 +10,57 @@ class OEXEngine {
   final String enginePath;
   late StreamController<String> _outStreamController;
   late StreamController<String> _errStreamController;
+  late Stream<String> _outStream;
+  late Stream<String> _errStream;
   late Process _process;
+  bool _isRunning = false;
 
-  get outStream => _outStreamController.stream;
-  get errStream => _errStreamController.stream;
+  bool get isRunning => _isRunning;
+  Stream<String> get outStream => _outStream;
+  Stream<String> get errStream => _errStream;
 
   OEXEngine(this.packageName, this.name, this.versionCode, this.fileName, this.enginePath);
 
   Future<Stream<String>> start() async {
+    if (_isRunning) throw Exception("Already running.");
     _outStreamController = new StreamController<String>();
     _errStreamController = new StreamController<String>();
     _process = await Process.start(this.enginePath, []);
     _process.stdout.listen(_stdout);
     _process.stderr.listen(_stderr);
+    _isRunning = true;
+
+    _outStream = _outStreamController.stream.asBroadcastStream();
+    _errStream = _errStreamController.stream.asBroadcastStream();
+
     return _outStreamController.stream;
   }
 
+  void stop() {
+    _outStreamController.close();
+    _errStreamController.close();
+    _process.kill();
+    _isRunning = false;
+  }
+
   void send(String message) {
+    if (!_isRunning) return;
     _process.stdin.add(utf8.encode(message + "\n"));
   }
 
   void _stdout(List<int> input) {
-    _outStreamController.add(utf8.decode(input));
+    if (!_isRunning) return;
+    String text = utf8.decode(input);
+    for (String line in text.split("\n")) {
+      _outStreamController.add(line);
+    }
   }
 
   void _stderr(List<int> input) {
-    _errStreamController.add(utf8.decode(input));
+    if (!_isRunning) return;
+    String text = utf8.decode(input);
+    for (String line in text.split("\n")) {
+      _errStreamController.add(line);
+    }
   }
 }
